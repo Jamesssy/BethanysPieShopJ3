db95703409d84d8a16f537bc4817c87441318896
James Johansson <shyco_2000@hotmail.com>
2017-05-26 01:37:00 +0200

    lagt till repositories och dbContext

--- BethsPieShop3/BethsPieShop3/Startup.cs
+++ BethsPieShop3/BethsPieShop3/Startup.cs
@@ -8,17 +8,37 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using BethsPieShop3.Models;
+using Microsoft.Extensions.Configuration;
+using Microsoft.EntityFrameworkCore;
 
 namespace BethsPieShop3
 {
     public class Startup
     {
+        private IConfigurationRoot _configurationRoot;
+
+        public Startup(IHostingEnvironment hostingEnviroment)
+        {
+
+            _configurationRoot = new ConfigurationBuilder()
+                .SetBasePath(hostingEnviroment.ContentRootPath)
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+
+        }
+
+       
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddTransient<ICategoryRepository, MockCategoryRepository>();
-            services.AddTransient<IPieRepository, MockPieRepository>();
+            services.AddDbContext<AppDbContext>(options =>
+            options.UseSqlServer(_configurationRoot.GetConnectionString("DefaultConnection")));
+
+            services.AddTransient<ICategoryRepository, CategoryRepository>();
+            services.AddTransient<IPieRepository, PieRepository>();
             services.AddMvc();
 
         }
