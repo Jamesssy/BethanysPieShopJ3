26f884552b7133ebdec6b40595e38aa5e3f82e96
James Johansson <shyco_2000@hotmail.com>
2017-05-25 17:20:44 +0200

    added alot

--- BethsPieShop3/BethsPieShop3/Startup.cs
+++ BethsPieShop3/BethsPieShop3/Startup.cs
@@ -7,31 +7,29 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using BethsPieShop3.Models;
 
 namespace BethsPieShop3
 {
     public class Startup
-    { test
+    {
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddTransient<ICategoryRepository, MockCategoryRepository>();
+            services.AddTransient<IPieRepository, MockPieRepository>();
+            services.AddMvc();
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
         {
-            loggerFactory.AddConsole();
-
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-
-            app.Run(async (context) =>
-            {
-                await context.Response.WriteAsync("Hello World!");
-            });
+            app.UseDeveloperExceptionPage();
+            app.UseStatusCodePages();
+            app.UseStaticFiles();
+            app.UseMvcWithDefaultRoute();
         }
     }
 }
---
+++ BethsPieShop3/BethsPieShop3/ViewModels/PiesListViewModel.cs
@@ -0,0 +1,16 @@
+﻿using BethsPieShop3.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BethsPieShop3.ViewModels
+{
+    public class PiesListViewModel
+    {
+        public IEnumerable<Pie> Pies { get; set; }
+
+        public string CurrentCategory { get; set; }
+
+    }
+}
